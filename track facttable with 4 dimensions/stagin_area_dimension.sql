fd7e77dfa05cd5ffa1eee59652114cec27edd827
Omer Yassir <omerym10@gmail.com>
2025-05-07 00:38:59 +0300

    Sorry

--- track facttable with 4 dimensions/stagin_area_dimension.sql
+++ track facttable with 4 dimensions/stagin_area_dimension.sql
@@ -1,3 +1,19 @@
+CREATE TABLE Customer_STG (
+    CustomerId INT NOT NULL,
+    FirstName NVARCHAR(40) NOT NULL,
+    LastName NVARCHAR(20) NOT NULL,
+    Company NVARCHAR(80) NULL,
+    Address NVARCHAR(70) NULL,
+    City NVARCHAR(40) NULL,
+    State NVARCHAR(40) NULL,
+    Country NVARCHAR(40) NULL,
+    PostalCode NVARCHAR(10) NULL,
+    Phone NVARCHAR(24) NULL,
+    Fax NVARCHAR(24) NULL,
+    Email NVARCHAR(60) NOT NULL,
+    SupportRepId INT NULL,
+    Created_date datetime
+);
 create table config(Name varchar(120),last_extract datetime) 
 create table artist_STG(ArtistId int primary key,Name varchar(120),created_date datetime)
 create table genre_STG(GenreId int primary key,Name varchar(120),created_date datetime)
